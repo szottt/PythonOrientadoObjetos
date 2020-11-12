a4030b71fa677a4c513a48a0ad2273dca0f9b969
szottt <szottt@hotmail.com>
2020-11-12 06:21:48 -0300

    Feita a primeira Classe

---
+++ main.py
@@ -0,0 +1,16 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Igor', 29)
+
+p2 = Pessoa('Marcela', 25)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
+
+# p1.falar('TI')
+# p2.falar('Arquitetura')
+
+#print(p1)
+#<pessoa.Pessoa object at 0x0000015098ADF940>
+#print(p2)
+#<pessoa.Pessoa object at 0x0000015098ADFA20>
